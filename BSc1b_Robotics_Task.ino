c2b6299198eb109cb6eea3d64723f41761d8f0c3
JCShiro <jacob.shadbolt@student.norwichuni.ac.uk>
2026-03-26 16:19:14 +0000

    first commit
    flashing LED, mad with universal timer

--- BSc1b_Robotics_Task.ino
+++ BSc1b_Robotics_Task.ino
@@ -1,9 +1,22 @@
+int LED_pin = 2;
+//LED period
+int LED_period = 1000;
+int last_LED_toggle = 0;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(LED_pin,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //get current time and compare with last toggle
+    int current_time = millis();
+    int time_since_last_toggle = current_time - last_LED_toggle;
 
+    //if it's time to toggle -> toggle and update variables
+    if(time_since_last_toggle > LED_period){
+      int current_LED_state = digitalRead(LED_pin);
+      digitalWrite(LED_pin, !current_LED_state);
+      last_LED_toggle = current_time;
+    }
 }
